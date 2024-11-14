56c3245ce684b2ca0bf8b24ebf719f54de8b8b3a
Alexsm111 <asm0083@alu.medac.es>
2024-11-14 18:41:52 +0100

    vista2

--- resources/views/cine.blade.php
+++ resources/views/cine.blade.php
@@ -7,16 +7,15 @@
   
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="text-center bg-gray-900 text-white">
+<body class="text-center bg-gray-600 text-white bg-cover bg-center" style="background-image: url('/imagenes/fondo1.png');">
+
   
   <div class="text-center mx-auto max-w-screen-lg">
-    <h1 class="text-4xl font-bold mb-8">Sala de Cine</h1>
+    <h1 class="text-4xl font-bold mb-8">SPIDERMAN</h1>
 
-    <div class="w-full h-40 bg-cover bg-center mt-6 mx-auto max-w-screen-md p-4" style="background-image: url('resources/imagenes/spiderman.png')">
-      </div>
-    
-    Pantalla
-  </div>
+    <img class="h-80 w-auto mx-auto" src="{{ asset('imagenes/spiderman.png') }}" alt="Spiderman">
+
+    </div>
   
   <div class="grid grid-cols-7 gap-2 mt-24 ml-6 mr-6">
     @for ($i = 0; $i < 70; $i++)
@@ -24,7 +23,8 @@
       @if ($i % 7 === 3)
         <div class="bg-transparent"></div>
       @else
-        <div class="bg-gray-500 rounded-full ">Disponible</div>
+        <div class="bg-blue-400 rounded-full" id="Asiento">Disponible</div>
+        
       @endif
     @endfor
   </div>

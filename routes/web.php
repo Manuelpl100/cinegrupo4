54e5d413f89daed2412cad96acc67d5859e9daff
diegojura <154268497+diegojura@users.noreply.github.com>
2024-11-15 17:38:02 +0100

    diego

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
+
 
 Route::get('/', function () {
     return view('cine');
 });
-=======
->>>>>>> 3ffcddba272a4c2d36272ee94c169215c6e70e25
+

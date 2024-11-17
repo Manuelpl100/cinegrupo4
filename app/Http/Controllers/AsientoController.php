df1983c09f0793f15c0d1c35af121394b66589b7
diegojura <154268497+diegojura@users.noreply.github.com>
2024-11-17 12:35:52 +0100

    diego controler v1

--- app/Http/Controllers/AsientoController.php
+++ app/Http/Controllers/AsientoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\asientos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class AsientoController extends Controller
 {
@@ -12,17 +13,65 @@ class AsientoController extends Controller
         return asientos::all();
     }
 
-    public function reservar(Request $request)
-    {
-    $request->validate([
-        'id' => 'required|exists:table_asientos,id',
-    ]);
-    $asiento = asientos::find($request->id);
+    public function reservar(Request $request){
+        $id_asiento = $request->id;
+        $asiento = asientos::find($id_asiento);
 
-    
+        if ($asiento == null){
+            $respuesta = array();
+            $respuesta['error']='No se encontro asiento';
+            return $respuesta;
+        }
+
+        if ($asiento -> disponibilidad == false){
+            $respuesta = array();
+            $respuesta['error']='El asiento ya esta ocupado';
+            return $respuesta;
+        }
+
+        $asiento->disponibilidad=false;
+        $asiento->save();
+        $respuesta=array();
+        $respuesta['mensaje']='Asiento reservado con éxito';
+        return $respuesta;
     }
 
+    public function buscarAsientoDisponibleAleatorio(){
+        $asientosDispo = asientos::all();
+        $asientosLib= array();
 
+        foreach ($asientosDispo as $asiento){
+            if ($asiento -> disponibilidad == true){
+                array_push($asientosLib, $asiento);
+            }
+        }
+
+        if (count($asientosLib)==0){
+            $respuesta = array();
+            $respuesta['error'] = 'No hay asientos disponibles';
+            return $respuesta;
+        }
+        $Aleatorio=rand(0, count($asientosLib) - 1);
+        $asientaco=$asientosLib[$Aleatorio];
+        return $asientaco;
+
+
+
+
+    }
+    public function contarAsientosOcupados(){
+        $todos=asientos::all();
+        $ocupados=0;
+
+        foreach($todos as $asiento){
+            if($asiento -> disponibilidad == false){
+                $ocupados=$ocupados+1;
+            }
+        }
+        $respuesta= array();
+        $respuesta['ocupados']=$ocupados;
+        return $respuesta;
+    }
 
 
 }

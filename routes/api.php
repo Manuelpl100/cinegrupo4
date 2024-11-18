63319fdd9ef7588e25c9f2a338e879dd188fc551
diegojura <154268497+diegojura@users.noreply.github.com>
2024-11-18 15:26:49 +0100

    api less

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-use App\Http\Controllers\AsientoController;
-
-Route::get('/asientos', [AsientoController::class, 'index']);
-Route::post('/asientos/reservar', [AsientoController::class, 'reservar']);
-Route::get('/asientos/aleatorio', [AsientoController::class, 'buscarAsientoDisponibleAleatorio']);
-Route::get('/asientos/ocupados', [AsientoController::class, 'contarAsientosOcupados']);

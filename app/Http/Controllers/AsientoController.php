e0caa07eb2f3c955dac40b46f5c9ab502b314e75
diegojura <154268497+diegojura@users.noreply.github.com>
2024-11-19 16:55:03 +0100

    intento de solucion de errores fallidos 3

--- app/Http/Controllers/AsientoController.php
+++ app/Http/Controllers/AsientoController.php
@@ -18,51 +18,30 @@ class AsientoController extends Controller
 
     public function reservar(Request $request, $id_sala)
     {
-<<<<<<< HEAD
-        $id_sala = $request->route('id');
-        $id_asientos = $request->input('id_asiento');
-
-        $asientos = asientos::select('id', 'disponibilidad')
-            ->whereIn('id', $id_asientos)
-            ->where('id_sala', $id_sala)
-            ->where('disponibilidad', true)
-            ->get();
-
-        if ($asientos->count() !== count($id_asientos)) {
-            return response()->json(['error' => 'Uno o más asientos no están disponibles o exceden el límite permitido'], 422);
-        }
-
-        $asientos->each(function ($asiento) {
-            $asiento->disponibilidad = false;
-            $asiento->save();
-        });
-
-=======
         $validator = Validator::make($request->all(), [
             'id_asiento' => 'required|array',
             'id_asiento.*' => 'integer|exists:asientos,id',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json(['error' => $validator->messages()], 422);
         }
-    
+
         $id_asientos = $request->input('id_asiento');
-    
+
         $asientos = asientos::whereIn('id', $id_asientos)
                             ->where('id_sala', $id_sala)
                             ->get();
-    
+
         if ($asientos->where('disponibilidad', false)->count()) {
             return response()->json(['error' => 'Uno o más asientos no están disponibles'], 422);
         }
-    
+
         foreach ($asientos as $asiento) {
             $asiento->disponibilidad = false;
             $asiento->save();
         }
-    
->>>>>>> 58e86a16f4b14e10956d702dbb85049a172357b5
+
         return response()->json(['mensaje' => 'Asiento(s) reservado(s) con éxito']);
     }
     public function buscarAsientoDisponibleAleatorio(Request $request)
